76fe56fc611155ad4bbe8b56a1c3757638af319f
Luiz Eduardo <luizedumartins21@gmail.com>
2022-08-23 16:29:52 -0300

    Instalação do banco de dados

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = "hello"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.permanent_session_lifetime = timedelta(days=10)
+
+db = SQLAlchemy(app)
+
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer)
+
 
 @app.route("/")
 def home():
     return redirect(url_for("user", usr="guest"))
 
+
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-         user = request.form["nm"]
-         return redirect(url_for("user",usr=user))
+        session.permanent = True
+        user = request.form["nm"]
+        session["user"] = user
+        return redirect(url_for("user"))
     else:
+        if "user" in session:
+            return redirect(url_for("user"))
         return render_template("login.html")
+
 
 @app.route("/cadastro", methods=["POST", "GET"])
 def cadastrar():
@@ -23,13 +42,29 @@ def cadastrar():
         return render_template("cadastro.html")
 
 
-@app.route("/<usr>")
-def user(usr):
-    return render_template("usuario.html", usr=usr)
+@app.route("/user")
+def user():
+    if "user" in session:
+        return render_template("usuario.html")
+    else:
+        return redirect(url_for("login"))
 
-@app.route("/<usr>/lista")
-def lista(usr):
-    return render_template("lista.html", usr=usr)
+
+@app.route("/user/lista")
+def lista():
+    return render_template("lista.html")
+
+
+@app.route("/user/estante")
+def estante():
+    return render_template("estante.html")
+
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
 
 if __name__ == "__main__":
     app.run(debug="True")
